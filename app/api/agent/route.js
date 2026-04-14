88f0d4604d27427514625ae52d60571891b083f6
NL45-E <nabilsangmaestro@gmail.com>
2026-04-15 06:44:15 +0700

    add telegram and special commnad "/" in AI agen

--- app/api/agent/route.js
+++ app/api/agent/route.js
@@ -1,19 +1,67 @@
-import { groqAI } from "../../../lib/groqAI.js";
-import { prompt } from "../../../lib/prompt.js";
+import { groqAI } from "@/lib/groqAI.js";
+import { prompt } from "@/lib/prompt.js";
+import { telegram } from "@/lib/telegram.js";
 export async function POST(req) {
-    const question = "Having problem with our website?";
+    let systemPrompt =
+      "You are a strict classifier in personal website. Return ONLY JSON."; 
+    let question = "Having problem with our website?";
     const { input } = await req.json();
-    if (!input) {
-        return Response.json({
-            reply:"input is required.",
-            option:"false"
-        });
+    function classify(inputText, include) {// iclude is array of words
+      const text = inputText.toLowerCase();
+      return include.every((word) => text.includes(word));
+    };
+    if (input[0] === "/") {
+      systemPrompt = "";
+      question = "";
+      const reply = await groqAI(input);
+      return Response.json({
+        reply: reply,
+        option: "false",
+        bug: ""
+      });
+    }
+    else if (!input) {
+      return Response.json({
+          reply:"input is required.",
+          option:"false"
+      });
+    }
+    // manual classification
+    else if (classify(input, ["cara", "login"]) 
+      || classify(input, ["cara", "masuk"]) 
+      || classify(input, ["how", "login"])) {
+      return Response.json({ 
+        reply: "are you already try to login?", option: "login" 
+      });
+    }
+    else if (classify(input, ["cara", "daftar"]) 
+      || classify(input, ["cara", "regis"]) 
+      || classify(input, ["how", "regis"])) {
+      return Response.json({ 
+        reply: "you can register here", option: "register" 
+      });
+    }
+    else if (classify(input, ["tentang", "web"]) 
+      || classify(input, ["about", "web"])
+      || classify(input, ["siapa", "web"])) {
+      return Response.json({ 
+        reply: "this is information about us", option: "info" 
+      });
     }
     // call groqAI to classify the input and generate a reply
-    const res = await groqAI(
-      prompt(question, input),
-      "You are a strict classifier in personal website. Return ONLY JSON.",
-    );
-    const { reply, option, bug } = JSON.parse(res);
-    return Response.json({ reply, option });
+    else {
+      const { reply, option, bug } = await groqAI(
+        prompt(question, input),
+        systemPrompt,
+      );
+      console.log("checkpoint one (1):  " + reply);
+      if (bug) {
+        await telegram("bug: " + bug);
+        return Response.json({
+          reply: `Bug sended. ${reply}`,
+          option: option,
+        });
+      }
+      return Response.json({ reply, option });
+    }
 }
--- app/page.js
+++ app/page.js
@@ -15,6 +15,7 @@ export default function Home() {
     });
     const {reply, option} = await response.json();
     setMessage(reply); setOption(option);
+    setInput("");
   };
   return (
     <>
