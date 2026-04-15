a2701659f934d55d1c22905f62ed3b6c7a9e9085
NL45-E <nabilsangmaestro@gmail.com>
2026-04-15 22:22:10 +0700

    design and add new field

--- app/page.js
+++ app/page.js
@@ -5,10 +5,17 @@ import style from "./style/main.module.css";
 export default function Home() {
   const [input, setInput] = useState("");
   const [message, setMessage] = useState("");
-  const [option, setOption] = useState("");
-  const [showInput, setShowInput] = useState(false); // New state for visibility
+  const [option, setOption] = useState("");// option is unimplemented
+  const [showLogButtons, setShowLogButtons] = useState(true);
+  const [showInput, setShowInput] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [logType, setLogType] = useState("login");
+  const [password, setPassword] = useState("");
 
-  async function agent() {
+  async function log(type) {// login and register
+    setShowLogButtons(false);// unimplemented
+  }
+  async function agent() {// agent
     if (!input) {
       setShowInput(false);
       return;
@@ -28,35 +35,77 @@ export default function Home() {
 
   return (
     <div className={style.container}>
-      <h1 className={style.title}>Login</h1>
+      {/* main menu (login and register) */}
 
-      <div className={style.content}>
-        <div>
-          {option === "login" && (
-            <button onClick={() => alert("login")} className={style.button}>
-              try login?
+      <div className={style.log}>
+        {showLogButtons && (
+          <div className={style.vertical}>
+            <h1 className={style.title}>Get Now</h1>
+            <button
+              className={style.logButton}
+              onClick={() => {
+                setLogType("register");
+                log();
+              }}
+            >
+              register
             </button>
-          )}
-          {option === "register" && (
-            <button className={style.button} onClick={() => alert("register")}>
-              try register?
+            <button
+              className={style.logButton}
+              onClick={() => {
+                setLogType("login");
+                log();
+              }}
+            >
+              Login
             </button>
-          )}
-          {option === "info" && (
-            <p>
-              This is a personal website including <i>AI agen</i> created by
-              Nabil.R
-              <br />
-              Source code:{" "}
-              <a href="https://github.com/NL45-E/AI-Project">github</a>
-              <br />
-              this website is <b>under development</b>
-            </p>
-          )}
-        </div>
+          </div>
+        )}
+
+        {/* input and register field */}
+
+        {!showLogButtons && (
+          <div className={style.vertical}>
+            <h1 className={style.title}>{logType}</h1>
+            <input
+              className={style.phoneInput}
+              type="number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value.toString())}
+            />
+            <div>
+              {/* OTP if register */}
+              {logType === "register" ? (
+                <button
+                  className={style.logButton}
+                  onClick={() => alert("OTP")}
+                >
+                  OTP
+                </button> // unimplemented
+              ) : (
+
+                // pw if user click login
+
+                <div className={style.vertical}>
+                  <input
+                    className={style.phoneInput}
+                    type="number"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value.toString())}
+                  />
+                  <button
+                    className={style.logButton}
+                    onClick={() => alert("pw submit")}
+                  > submit
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </div>
 
-      {/* Chat bar at the bottom */}
+      {/* AI Chat bar at the bottom */}
 
       <div className={style.bar}>
         <p className={showInput && message ? style.message : ""}>
